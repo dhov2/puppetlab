221d310fb5529fafef72bd56957d448429a76d4c
Thomas Védrinelle <thomas.vedrinelle@gmail.com>
2021-07-08 14:37:26 +0000

    TP1-variable

--- wiki3.pp
+++ wiki3.pp
@@ -19,6 +19,7 @@ exec {
   'unzip-dokuwiki':
     path    => ['/usr/bin/'],
     command => 'tar -xavf dokuwiki.tgz',
+    creates => '/usr/src/dokuwiki-2020-07-29',
     cwd     => '/usr/src',
     require => File['download-dokuwiki'];
 }
@@ -31,21 +32,13 @@ file {
     require => Exec['unzip-dokuwiki'];
 }
 
+$document_root = recettes.conf
+$server_name = www.recettes.com
+
 file {
-  'create-dir-recettes':
+  'create-dir-$document_root':
     ensure  => 'present',
-    path    => '/var/www/recettes.wiki/',
-    source  => '/usr/src/dokuwiki',
-    ignore  => '/var/www/',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-    require => File['rename-dokuwiki'];
-}
-file {
-  'create-dir-politique':
-    ensure  => 'present',
-    path    => '/var/www/politique.wiki/',
+    path    => '/var/www/$document_root/',
     source  => '/usr/src/dokuwiki',
     ignore  => '/var/www/',
     owner   => 'www-data',
@@ -55,63 +48,36 @@ file {
 }
 
 file {
-  $document_root = recettes.conf
   'config-$document_root':
     ensure  => 'present',
-    source  => '/home/vagrant/puppetlab/site.conf',
-    path    => '/etc/apache2/sites-available/$document_root',
-    require => Package['install-apache2'];
-}
-file {
-  $document_root = politique.conf
-  'config-$document_root':
-    ensure  => 'present',
-    source  => '/home/vagrant/puppetlab/site.conf',
+    content => template('/home/vagrant/puppetlab/site.conf)',
     path    => '/etc/apache2/sites-available/$document_root',
     require => Package['install-apache2'];
 }
 
 exec {
-  $document_root = recettes.conf
-  'modify-conf-$document_root':
-    command => "template('/home/vagrant/puppetlab/site.conf')",
-    path    => '/etc/apache2/sites-available/$document_root';
-}
-exec {
-  $document_root = politique.conf
   'modify-conf-$document_root':
     command => "template('/home/vagrant/puppetlab/site.conf')",
     path    => '/etc/apache2/sites-available/$document_root';
 }
 
 exec {
-  $document_root = recettes.conf
   'enable-vhosts-$document_root':
-    path    => '/usr/bin',
+    path    => ['/usr/bin'],
     cwd     => '/etc/apache2/sites-available/',
     command => 'a2ensite $document_root',
     require => Exec['config-$document_root'],
     notify  => Service['reload-apache2'];
 }
-exec {
-  $document_root = politique.conf
-  'enable-vhosts-$document_root';
-    path    => '/usr/bin',
-    cwd     => '/etc/apache2/sites-available/',
-    command => 'a2ensite $document_root',
-    require => Exec['config-document_root'];
-}
 
 service {
   'reload-apache2':
     ensure    => 'running',
-    name      => 'apache2';
+    name      => 'apache2',
     subscribe => Exec['enable-vhosts-$document_root];
 }
 
 host {
-  '$server_name':
-    ip => '127.0.0.1';
   '$server_name':
     ip => '127.0.0.1';
 }
