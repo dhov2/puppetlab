788432e53687227ad9c0fd80711a6527ab76e780
Thomas Védrinelle <thomas.vedrinelle@gmail.com>
2021-07-09 08:09:20 +0000

    TP1-nodes-classes

--- wiki4.pp
+++ wiki4.pp
@@ -1,166 +1,93 @@
-package {
-  "install-apache2":
-    ensure => 'present',
-    name   => 'apache2';
-  "install-php7.3":
-    ensure => 'present',
-    name   => 'php7.3';
+node 'server0' {
+  include hosting
+  $server_name = www.politique.wiki
+  $document_root = politique.conf
+}
+node 'server1' {
+  include hosting
+  $server_name = www.recettes.wiki
+  $document_root = recettes.conf
 }
 
-file {
-  "download-dokuwiki":
-    ensure         => 'present',
-    source         => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
-    path           => '/usr/src/dokuwiki.tgz',
-    checksum_value => '8867b6a5d71ecb5203402fe5e8fa18c9';
-}
+class hosting {
+  package {
+    "install-apache2":
+      ensure => 'present',
+      name   => 'apache2';
+    "install-php7.3":
+      ensure => 'present',
+      name   => 'php7.3';
+  }
 
-exec {
-  "unzip-dokuwiki":
-    path    => ['/usr/bin/'],
-    command => 'tar -xavf dokuwiki.tgz',
-    creates => '/usr/src/dokuwiki-2020-07-29',
-    cwd     => '/usr/src',
-    require => File['download-dokuwiki'];
-}
+  file {
+    "download-dokuwiki":
+      ensure         => 'present',
+      source         => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
+      path           => '/usr/src/dokuwiki.tgz',
+      checksum_value => '8867b6a5d71ecb5203402fe5e8fa18c9';
+  }
 
-file {
-  "rename-dokuwiki":
-    ensure  => 'present',
-    source  => '/usr/src/dokuwiki-2020-07-29',
-    path    => '/usr/src/dokuwiki',
-    require => Exec['unzip-dokuwiki'];
-}
+  exec {
+    "unzip-dokuwiki":
+      path    => ['/usr/bin/'],
+      command => 'tar -xavf dokuwiki.tgz',
+      creates => '/usr/src/dokuwiki-2020-07-29',
+      cwd     => '/usr/src',
+      require => File['download-dokuwiki'];
+  }
 
-$document_root = 'recettes.conf'
-$server_name = 'www.recettes.com'
+  file {
+    "rename-dokuwiki":
+      ensure  => 'present',
+      source  => '/usr/src/dokuwiki-2020-07-29',
+      path    => '/usr/src/dokuwiki',
+      require => Exec['unzip-dokuwiki'];
+  }
 
-file {
-  "create-dir-${document_root}":
-    ensure  => 'present',
-    path    => '/var/www/${document_root}',
-    source  => '/usr/src/dokuwiki',
-    ignore  => '/var/www/',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-    require => File['rename-dokuwiki'];
-}
+  file {
+    "create-dir-${document_root}":
+      ensure  => 'present',
+      path    => '/var/www/${document_root}',
+      source  => '/usr/src/dokuwiki',
+      ignore  => '/var/www/',
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0755',
+      require => File['rename-dokuwiki'];
+  }
 
-file {
-  "config-${document_root}":
-    ensure  => 'present',
-    content => template('/home/vagrant/puppetlab/template/site.conf.erb)',
-    path    => '/etc/apache2/sites-available/${document_root}',
-    require => Package['install-apache2'];
-}
+  file {
+    "config-${document_root}":
+      ensure  => 'present',
+      content => template('/home/vagrant/puppetlab/template/site.conf.erb)',
+      path    => '/etc/apache2/sites-available/${document_root}',
+      require => Package['install-apache2'];
+  }
 
-exec {
-  "modify-conf-${document_root}":
-    command => "template('/home/vagrant/puppetlab/template/site.conf.erb')",
-    path    => '/etc/apache2/sites-available/${document_root}';
-}
+  exec {
+    "modify-conf-${document_root}":
+      command => "template('/home/vagrant/puppetlab/template/site.conf.erb')",
+      path    => '/etc/apache2/sites-available/${document_root}';
+  }
 
-exec {
-  "enable-vhosts-${document_root}":
-    path    => ['/usr/bin'],
-    cwd     => '/etc/apache2/sites-available/',
-    command => 'a2ensite $document_root',
-    require => Exec['config-${document_root}'],
-    notify  => Service['reload-apache2'];
-}
+  exec {
+    "enable-vhosts-${document_root}":
+      path    => ['/usr/bin'],
+      cwd     => '/etc/apache2/sites-available/',
+      command => 'a2ensite $document_root',
+      require => Exec['config-${document_root}'],
+      notify  => Service['reload-apache2'];
+  }
 
-service {
-  'reload-apache2':
-    ensure    => 'running',
-    name      => 'apache2',
-    subscribe => Exec['enable-vhosts-${document_root}'];
-}
+  service {
+    'reload-apache2':
+      ensure    => 'running',
+      name      => 'apache2',
+      subscribe => Exec['enable-vhosts-${document_root}'];
+  }
 
-host {
-  "${server_name}":
-    ip => '127.0.0.1';
-}
-package {
-  "install-apache2":
-    ensure => 'present',
-    name   => 'apache2';
-  "install-php7.3":
-    ensure => 'present',
-    name   => 'php7.3';
-}
-
-file {
-  "download-dokuwiki":
-    ensure         => 'present',
-    source         => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz',
-    path           => '/usr/src/dokuwiki.tgz',
-    checksum_value => '8867b6a5d71ecb5203402fe5e8fa18c9';
-}
-
-exec {
-  "unzip-dokuwiki":
-    path    => ['/usr/bin/'],
-    command => 'tar -xavf dokuwiki.tgz',
-    creates => '/usr/src/dokuwiki-2020-07-29',
-    cwd     => '/usr/src',
-    require => File['download-dokuwiki'];
-}
-
-file {
-  "rename-dokuwiki":
-    ensure  => 'present',
-    source  => '/usr/src/dokuwiki-2020-07-29',
-    path    => '/usr/src/dokuwiki',
-    require => Exec['unzip-dokuwiki'];
-}
-
-$document_root = 'recettes.conf'
-$server_name = 'www.recettes.com'
-
-file {
-  "create-dir-${document_root}":
-    ensure  => 'present',
-    path    => '/var/www/${document_root}',
-    source  => '/usr/src/dokuwiki',
-    ignore  => '/var/www/',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-    require => File['rename-dokuwiki'];
-}
-
-file {
-  "config-${document_root}":
-    ensure  => 'present',
-    content => template('/home/vagrant/puppetlab/template/site.conf.erb)',
-    path    => '/etc/apache2/sites-available/${document_root}',
-    require => Package['install-apache2'];
-}
-
-exec {
-  "modify-conf-${document_root}":
-    command => "template('/home/vagrant/puppetlab/template/site.conf.erb')",
-    path    => '/etc/apache2/sites-available/${document_root}';
-}
-
-exec {
-  "enable-vhosts-${document_root}":
-    path    => ['/usr/bin'],
-    cwd     => '/etc/apache2/sites-available/',
-    command => 'a2ensite $document_root',
-    require => Exec['config-${document_root}'],
-    notify  => Service['reload-apache2'];
-}
-
-service {
-  'reload-apache2':
-    ensure    => 'running',
-    name      => 'apache2',
-    subscribe => Exec['enable-vhosts-${document_root}'];
-}
-
-host {
-  "${server_name}":
-    ip => '127.0.0.1';
+  host {
+    "${server_name}":
+      ip => '127.0.0.1';
+  }
 }
